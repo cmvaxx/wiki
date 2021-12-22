d69ffe9d3cbaee1d83931b191df03fd4abc849f2
cmvaxx <87703683@qq.com>
2021-12-22 21:34:30 +0800

    成功测试了helloworld接口

--- src/main/java/com/cmvaxx/wiki/config/WikiApplication.java
+++ src/main/java/com/cmvaxx/wiki/config/WikiApplication.java
@@ -1,8 +1,10 @@
-package com.cmvaxx.wiki;
+package com.cmvaxx.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.cmvaxx")
 @SpringBootApplication
 public class WikiApplication {
 
