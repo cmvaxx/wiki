8e4e945e3d9e635b796ea1f277f7d78905f022a6
cmvaxx <87703683@qq.com>
2022-01-04 12:48:03 +0800

    集成Mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,13 @@ create table `test`(
                        `password` varchar(50) comment '密码',
                        primary key (`id`)
 )engine = innodb default charset = utf8mb4 comment = '测试';
+insert into `test`(id,name,password) value (1,'测试','password');
+
+
+drop table if exists `Demo`;
+create table `Demo`(
+                       `id` bigint not null comment 'id',
+                       `name` varchar(50) comment '名称',
+                       primary key (`id`)
+)engine = innodb default charset = utf8mb4 comment = '测试';
+insert into `Demo`(id,name) value (1,'测试');
